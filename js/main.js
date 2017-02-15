deec16d18a3fb72a16505ce4dbfb1fdb4ca37b14
Anouk Ruhaak <anoukruhaak@gmail.com>
2017-02-15 21:24:24 +0100

    adds css

--- js/main.js
+++ js/main.js
@@ -9,12 +9,8 @@
  	answer_a = s.select("g[id='a']"),
  	answer_b = s.select("g[id='b']"),
  	answer_c = s.select("g[id='c']"),
- 	text_a = s.select("text[id='antwoord-a']"),
- 	text_b = s.select("text[id='antwoord-b']"),
- 	text_c = s.select("text[id='antwoord-c']"),
  	trophee1 = s.select("g[id='kikker-beker']"),
  	koffer = s.select("g[id='koffer']"),
- 	vraag = s.select("g[id='vraag']"),
  	newsText = s.select("g[id='news-tekst']");
 
 class Answer {
@@ -37,49 +33,52 @@ class NewsText {
 }
 
 class State {
-	constructor(earth, business){
+	constructor(earth, business, news_ids){
 		this.earth = earth;
 		this.business = business;
-		this.news_ids = [];
+		this.news_ids = news_ids;
 	}
 }
 
-var global_state = new State(0, 0),
- 	states = [new State(0,0)],
+var global_state = new State(0, 0, []),
+ 	states = [],
  	selected_question = 0,
  	foodDone = false,
  	homeDone = false,
  	careDone = false;
 
 
-var newsArray = [new NewsText(0, "IJsberen verdrinken door smeltende polen."), 
-new NewsText(1, "Door het warme weer neemt de vraag ijs toe."), 
-new NewsText(2, "Laatste top 8 resulteert in ruzie en geweld. Einde van de wereld nu snel in zicht.")];
+var newsArray = [new NewsText(0, "Nieuwe CEO Unilever gooit het roer om."),
+new NewsText(1, "Groene stroom in fabrieken Unilever!"),
+new NewsText(2, "Unilever speelt op zeker: nieuwe CEO durft groene stroom nog niet aan."),
+new NewsText(3, "Unilever haalt voor 2020 al het vlees uit haar producten. Consumenten reageren geschokt."), 
+new NewsText(4, "De vegetarisch bond is blij met de vegetarische Unox worsten!")
+];
 
-var questions = [{"id": 0, "text": "gender", "answerA": new Answer(0.1, "A. Man", 0, 0, 0, 0, 1), 
-"answerB": new Answer(0.2, "B. Vrouw", 0, 0, 0, 0, 0), "answerC": new Answer(0.3, "C. Ik twijfel", 0, 0, 0, 0, 0)},
+var questions = [{"id": 0, "text": "Ik ben een:", "answerA": new Answer(0.1, "Man", 0, 0, 0, 0, 1), 
+"answerB": new Answer(0.2, "Vrouw", 0, 0, 0, 0, 0), "answerC": new Answer(0.3, "Ik twijfel", 0, 0, 0, 0, 0)},
 
-{"id": 1, "text": "CEO", "answerA": new Answer(1.1, "Groen", 0, 0, 1, 0, 2), 
-"answerB": new Answer(1.2, "Fossiel", 0, 0, 1, 0, 17), 
+{"id": 1, "text": "Gefeliciteerd! Je bent topman van Unilever. De planeet kreunt onder jouw productie. Je eerste besluit:", "answerA": new Answer(1.1, "Ik gooi het roer om en kies voor een volledig duurzame productie", 1, 0, 0, 0, 2), 
+"answerB": new Answer(1.2, "Ik kan dit niet oplossen. We onderzoeken vergroening maar draaien voorlopig op de oude voet door.", -1, 1, 2, 0, 17), 
 "answerC": null}, 
 
 //Start Groen
-{"id": 2, "text": "Fabriek", "answerA": new Answer(2.1, "Duurzaam", 1, -1, 0, 2, 3), 
-"answerB": new Answer(2.2, "Fossiel", 1, -1, 0, 0, 17), 
+{"id": 2, "text": "Mooi. Laat je de fabrieken volledig draaien op duurzame energie?", "answerA": new Answer(2.1, "Ja, ik stap direct over op volledig groene stroom en bio-brandstof.", 2, 2, 0, 0, 3), 
+"answerB": new Answer(2.2, "Groene subsidies zijn onzeker; ik begin met 10 procent groen.", 0, 0, 2, 0, 17), 
 "answerC": null},
 
 //Keuze
-{"id": 3, "text": "Keuze", "answerA": new Answer(3.1, "Food", 0, 0, 0, 2, 4), 
-"answerB": new Answer(3.2, "Personal Care", 0, 0, 0, 0, 9), 
-"answerC": new Answer(3.3, "Home", 0, 0, 0, 0, 11)},
+{"id": 3, "text": "Welke divisie pak je als eerste aan?", "answerA": new Answer(3.1, "Voeding", 0, 0, 0, 0, 4), 
+"answerB": new Answer(3.2, "Cosmetica en verzorgingsproducten", 0, 0, 0, 0, 9), 
+"answerC": new Answer(3.3, "Wasmiddelen en reiniging", 0, 0, 0, 0, 11)},
 
 //Food groen
-{"id": 4, "text": "Food 1 (vraag 4)", "answerA": new Answer(4.1, "antwoord 1", 1, -1, 0, 2, 5), 
-"answerB": new Answer(4.2, "skip 5", 1, 1, 0, 0, 6), 
-"answerC": new Answer(4.3, "skip 5", 1, -1, 0, 0, 6)},
+{"id": 4, "text": "Je verkoopt worst en ijs, maar vlees en zuivel zijn een zware belasting voor het milieu. Je besluit:", "answerA": new Answer(4.1, "Voor 2020 alle vlees en zuivel te vervangen door veganistische alternatieven", 5, -2, 3, 0, 6), 
+"answerB": new Answer(4.2, "Een vegetarische variant op de Unox-worst te introduceren", 0, 0, 0, 0, 5),
+"answerC": new Answer(4.3, "Samen te werken met het Vegetarisch Verbond, maar niet minder vlees te verkopen.",-2, 4, 0, 0, 5)},
 
-{"id": 5, "text": "Food 2 (vraag 5)", "answerA": new Answer(5.1, "Duurzaam", 1, 1, 0, 2, 6), 
-"answerB": new Answer(5.2, "Fossiel", 1, 1, 0, 0, 6), 
+{"id": 5, "text": "Weet je het zeker? Unox is cultureel erfgoed!", "answerA": new Answer(5.1, "Klopt, ik kies voor zeker. Unox blijft Unox.", -2, 3, 0, 0, 6), 
+"answerB": new Answer(5.2, "Ik blijf bij mijn besluit: vegetarische worst.", 3, -2, 4, 1, 6), 
 "answerC": null},
 
 {"id": 6, "text": "Food 3 (vraag 6)", "answerA": new Answer(6.1, "Duurzaam", 1, -1, 0, 2, 7), 
@@ -214,29 +213,23 @@ function displayDashboard(id) {
 	console.log(homeDone, careDone, foodDone);
 	selected_question = id;
 
-	if (homeDone) {
-		colorButton(answer_c, "green");
-	}
+	if (homeDone) colorButton(answer_c, "green");
+	if (careDone) colorButton(answer_b, "green");
+	if (foodDone) colorButton(answer_a, "green");
 	
-	if (careDone) {
-		colorButton(answer_b, "green");
-	}
-	
-
-	if (foodDone) {
-		colorButton(answer_a, "green");
-	}
 
 	var txt = questions[id].text,
 		a = questions[id].answerA.text,
 		b = questions[id].answerB.text,
 		c = questions[id].answerC.text; 
-	
-	vraag.select("text").attr({ text: txt});
 	answer_c.attr({display: ''});
-	text_a.attr({ text: a});
-	text_b.attr({ text: b});
-	text_c.attr({ text: c});
+
+	document.getElementById('vraag').innerHTML = txt;
+
+
+	// text_a.attr({ text: "A. "+ a});
+	// text_b.attr({ text: "B. " +b});
+	// text_c.attr({ text: "C. "+c});
 }
 
 function goToLoading() {
@@ -265,18 +258,19 @@ function displayQuestion(id) {
 		a = questions[id].answerA.text,
 		b = questions[id].answerB.text,
 		c = questions[id].answerC; 
+	document.getElementById('vraag').innerHTML = txt;
 	
-	if (c === null) {
-		text_c.attr({ text: ""});
-		answer_c.attr({display: 'none'});
-	} else {
-		text_c.attr({ text: c.text});
-		answer_c.attr({display: ''});
-	}
+	// if (c === null) {
+	// 	text_c.attr({ text: ""});
+	// 	answer_c.attr({display: 'none'});
+	// } else {
+	// 	text_c.attr({ text: "C. " + c.text});
+	// 	answer_c.attr({display: ''});
+	// }
 
-	vraag.select("text").attr({ text: txt});
-	text_a.attr({ text: a});
-	text_b.attr({ text: b});
+	// vraag.select("text").attr({ text: txt});
+	// text_a.attr({ text: "A. " +a});
+	// text_b.attr({ text:  "B. " + b});
 	
 };
 
@@ -323,7 +317,10 @@ function updateState(){
 	if (current_state !=  null) {
 		global_state.earth += current_state.earth;
 		global_state.business += current_state.business;
-		updateNewsBanner(current_state);
+
+		if (current_state.news_ids.length > 0) {
+			updateNewsBanner(current_state);
+		}
 	}
 }
 
@@ -360,6 +357,8 @@ function setUp(){
 	help.mouseover(animateHelpButton,resetSVG);
 	trophee1.drag();
 	koffer.drag();
+	newsText.select("text").attr({"font-size": "16px", "font-family": "Catamaran, sans-serif", "font-style": "regular"});
+
 	// var animBack = function () {koffer.animate({transform: "rotate(20 100 0)"}, 2000, mina.easeinout, null)};
 	// var anim = function () {koffer.animate({transform: "rotate(20 100 100 )"}, 2000, mina.easeinout, animBack)};
 	// anim();
@@ -404,7 +403,7 @@ function setUp(){
 		startMatrix.translate(1000, 0);
 		midMatrix.translate(-100, 0);
 
-		newsText.animate({opacity: 0.7, transform: midMatrix}, 7000, mina.linear, function () {
+		newsText.animate({opacity: 0.7, transform: midMatrix}, 9000, mina.linear, function () {
 			newsText.animate({opacity: 1.0, transform: startMatrix}, 1, mina.easeout, function () {
 				moveNewsBanner();
 			});
