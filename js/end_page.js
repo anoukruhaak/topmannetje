2b733786726c5c0086863b3d6bca4044160f4d8d
Anouk Ruhaak <anoukruhaak@gmail.com>
2017-03-08 18:43:49 +0100

    facebook share enabled

--- js/end_page.js
+++ js/end_page.js
@@ -1,8 +1,8 @@
 var loading = Snap("#loading"),
 	end_page = Snap("#end-page"),
-	url = "http://topmannetje.herokuapp.com",
-	text = "Wat voor topmannetje ben jij? Ga jij voor het snelle geld? Of zet je je bedrijf in om de planeet te redden?",
-	titel = "Speel topmannetje!";
+	text = "&description=Wat voor topmannetje ben jij? Ga jij voor het snelle geld? Of zet je je bedrijf in om de planeet te redden?",
+	title = "&title=Speel topmannetje!",
+	caption = "&caption=Platform Investio";
 
 function transitionToEndPage(){
 	var timer = setTimeout( function() {
@@ -15,6 +15,10 @@ function transitionToEndPage(){
 }
 
 function setUp(){
+	var url = window.location.pathname,
+	urlsplit = url.split("/").slice(-1)[0];
+
+
 	var arrow = end_page.select("text[id='button']"),
 		fb = end_page.select("path[id='fb']"),
 		twitter = end_page.select("g[id='twitter']");
@@ -23,17 +27,40 @@ function setUp(){
 		window.location.href = "http://www.platform-investico.nl";
 	};
 
+	fb.node.onclick = function () {
+		switch (urlsplit) {
+			case "appelboer.html":
+				goToFacebook("appelboer.jpg");
+				break;
+			case "duurzaam.html":
+				goToFacebook("duurzaam.jpg");
+				break;
+			case "reputatie.html":
+				goToFacebook("baas.jpg");
+				break;
+			case "speel_op_zeker":
+				goToFacebook("reputatiemanager.jpg");
+				break;
+			default:
+				goToFacebook("reputatiemanager.jpg");
+				break;
+		}
+	}
 
-	function goToFacebook() {
-		var pathname = window.location.pathname;
-		console.log(pathname);
-		
-		window.location.href ="https://www.facebook.com/sharer/sharer.php?u=ikstem.com&picture=http%3A%2F%2Ftopmannetje.herokuapp.com%2Fimg%2Fappelboer.jpg&title=Hallo&caption=Bye&quote=&description=Jij+verkoopt+liever+appels+op+de+markt."
+
+	function goToFacebook(img) {
+		var url = "https://www.facebook.com/sharer/sharer.php?u=topmannetje.herokuapp.com&title=Wat+voor+topman+ben+jij%3F&caption=Platform+Investico&quote=&description=Wat+voor+topmannetje+ben+jij%3F+Ga+jij+voor+het+snelle+geld%3F+Of+zet+je+je+bedrijf+in+om+de+planeet+te+redden%3F";
+		var picture = "&picture=http%3A%2F%2Ftopmannetje.herokuapp.com%2Fimg%2F"+ img 
+		console.log(url);
+		window.location.href = url + picture;
 	}
 
 	function goToTwitter() {
 
 	}
 }
+
+// https://www.facebook.com/sharer/sharer.php?undefined&picture=http://topmannetje.herokuapp.com/img/appelboer.jpg&title=Speel%20topmannetje!&caption=Platform%20Investio
+// https://www.facebook.com/sharer/sharer.php?u=ikstem.com&picture=http%3A%2F%2Ftopmannetje.herokuapp.com%2Fimg%2Fappelboer.jpg&title=Hallo&caption=Bye&quote=&description=Jij+verkoopt+liever+appels+op+de+markt.
 
 transitionToEndPage();
